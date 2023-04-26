a74da1b9c9afed33b4a3b9bc6c065496860d81f2
Carlos Afonso <afonso.carlos.braga@gmail.com>
2023-04-26 09:55:25 +0100

    Enable Caps Word

--- keyboards/crkbd/keymaps/afonsocarlos/config.h
+++ keyboards/crkbd/keymaps/afonsocarlos/config.h
@@ -24,3 +24,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MASTER_LEFT
 // #define MASTER_RIGHT
 // #define EE_HANDS
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+#define HOLD_ON_OTHER_KEY_PRESS
--- keyboards/crkbd/keymaps/afonsocarlos/rules.mk
+++ keyboards/crkbd/keymaps/afonsocarlos/rules.mk
@@ -1,3 +1,4 @@
 MOUSEKEY_ENABLE     = no     # Mouse keys
 VIA_ENABLE          = yes         # Enable VIA
 
+CAPS_WORD_ENABLE    = yes
