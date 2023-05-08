f02651c65b26d29f77b7347561d212af1e46d460
Carlos Afonso <afonso.carlos.braga@gmail.com>
2023-05-08 17:14:31 +0100

    Enable RGB lighting config

--- keyboards/crkbd/keymaps/afonsocarlos/rules.mk
+++ keyboards/crkbd/keymaps/afonsocarlos/rules.mk
@@ -1,10 +1,11 @@
 SRC += features/achordion.c
+SRC += rgb_config.c
 
 CAPS_WORD_ENABLE    = yes
 COMBO_ENABLE        = yes
 KEY_OVERRIDE_ENABLE = yes
 LTO_ENABLE          = yes
-MOUSEKEY_ENABLE     = yes
+RGBLIGHT_ENABLE     = yes
 # TAP_DANCE_ENABLE = yes
 # WPM_ENABLE          = yes
 
@@ -18,7 +19,6 @@ MAGIC_ENABLE      = no
 MIDI_ENABLE       = no
 MOUSEKEY_ENABLE   = no
 NKRO_ENABLE       = no
-RGBLIGHT_ENABLE   = no
 RGB_MATRIX_ENABLE = no
 SLEEP_LED_ENABLE  = no
 SPACE_CADET_ENABLE= no
