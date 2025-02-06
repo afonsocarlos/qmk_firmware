ac50ad894c18f7eb91fa80903577552dc7d200a1
Carlos Afonso <afonso.carlos.braga@gmail.com>
2025-02-06 13:03:38 +0000

    Implement "repeat key" on hold for custom shortcuts

--- keyboards/crkbd/keymaps/afonsocarlos/features/shortcuts.c
+++ keyboards/crkbd/keymaps/afonsocarlos/features/shortcuts.c
@@ -11,6 +11,19 @@
             return false;                                         \
         }
 
+#define OS_REGISTER(macro_key, mac_tap_key, linux_tap_key)                 \
+    case macro_key:                                                        \
+        if (record->event.pressed) {                                       \
+            if (record->tap.count > 0) { /* It's a tap */                  \
+                tap_code16(is_macos() ? mac_tap_key : linux_tap_key);      \
+            } else { /* It's a hold */                                     \
+                register_code16(is_macos() ? mac_tap_key : linux_tap_key); \
+            }                                                              \
+            return false;                                                  \
+        }                                                                  \
+        unregister_code16(is_macos() ? mac_tap_key : linux_tap_key);       \
+        return true;
+
 bool process_shortcuts(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         // Close Window
@@ -29,7 +42,7 @@ bool process_shortcuts(uint16_t keycode, keyrecord_t *record) {
         OS_TAP(MC_PASTE, G(KC_V), C(KC_V))
 
         // Delete word
-        OS_TAP(MC_DELWRD, A(KC_BSPC), C(KC_BSPC))
+        OS_REGISTER(MC_DELWRD, A(KC_BSPC), C(KC_BSPC))
     }
 
     return true;
