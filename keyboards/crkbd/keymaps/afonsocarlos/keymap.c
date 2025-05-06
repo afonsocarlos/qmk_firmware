f376bc434d9e0912d91f11868bcccc0be3d165fd
Carlos Afonso <afonso.carlos.braga@gmail.com>
2025-05-06 23:33:12 +0100

    Re-enable retro shift for modifiers

--- keyboards/crkbd/keymaps/afonsocarlos/keymap.c
+++ keyboards/crkbd/keymaps/afonsocarlos/keymap.c
@@ -346,8 +346,7 @@ void autoshift_press_user(uint16_t keycode, bool shifted, keyrecord_t *record) {
                 add_weak_mods(MOD_BIT(KC_LSFT));
             }
             // & 0xFF gets the Tap key for Tap Holds, required when using Retro Shift
-            // register_code16((IS_RETRO(keycode)) ? keycode & 0xFF : keycode);
-            register_code16(keycode);
+            register_code16((IS_RETRO(keycode)) ? keycode & 0xFF : keycode);
     }
 }
 
@@ -375,8 +374,7 @@ void autoshift_release_user(uint16_t keycode, bool shifted, keyrecord_t *record)
             // & 0xFF gets the Tap key for Tap Holds, required when using Retro Shift
             // The IS_RETRO check isn't really necessary here, always using
             // keycode & 0xFF would be fine.
-            // unregister_code16((IS_RETRO(keycode)) ? keycode & 0xFF : keycode);
-            unregister_code16(keycode);
+            unregister_code16((IS_RETRO(keycode)) ? keycode & 0xFF : keycode);
     }
 }
 
